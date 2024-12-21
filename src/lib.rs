860d3e57fa60e5e5a7c91aa56bfa2bc27a7f03c7
advanced-user <stolbetskiy.yura@gmail.com>
2024-12-21 13:14:04 +0200

    fix key overflow

--- src/lib.rs
+++ src/lib.rs
@@ -35,16 +35,11 @@ pub fn caesar_encrypt(text: &str, key: i32, lang: Language) -> String {
         };
 
         if let Some(pos) = alphabet.iter().position(|&letter| letter == c) {
-            let len = alphabet.len() as i32;
+            let alph_len = alphabet.len() as i32;
             let mut shifted_pos = pos as i32 + key;
 
-            if shifted_pos < 0 {
-                shifted_pos += len;
-            } else if shifted_pos >= len {
-                shifted_pos -= len;
-            }
-
-            return alphabet[shifted_pos as usize];
+            shifted_pos = shifted_pos.rem_euclid(alph_len); // like % in python
+            return  alphabet[shifted_pos as usize];
         }
 
         return c;
@@ -69,9 +64,25 @@ mod tests {
     }
 
     #[test]
+    fn encrypt_eng_text_key_overflow() {
+        let plain_text = "aBcdEFgHiJkLmNoPqRSTuVWxYz";
+        let key = 27;
+        let encrypted_text = caesar_encrypt(&plain_text, key, Language::English);
+        assert_eq!(encrypted_text, "bCdeFGhIjKlMnOpQrSTUvWXyZa");
+    }
+
+    #[test]
     fn decrypt_eng_text() {
         let encrypted_text = "eFghIJkLmNoPqRsTuVWXyZAbCd";
         let key = 4;
+        let dencrypted_text = caesar_decrypt(&encrypted_text, key, Language::English);
+        assert_eq!(dencrypted_text, "aBcdEFgHiJkLmNoPqRSTuVWxYz");
+    }
+
+    #[test]
+    fn decrypt_eng_text_overflow() {
+        let encrypted_text = "bCdeFGhIjKlMnOpQrSTUvWXyZa";
+        let key = 27;
         let dencrypted_text = caesar_decrypt(&encrypted_text, key, Language::English);
         assert_eq!(dencrypted_text, "aBcdEFgHiJkLmNoPqRSTuVWxYz");
     }
@@ -85,9 +96,25 @@ mod tests {
     }
 
     #[test]
+    fn encrypt_ukr_text_overflow() {
+        let plain_text = "АбВгҐДеЄЖЗИіЇЙКлМНоПРСтУфХЦчШЩЬЮя";
+        let key = 35;
+        let encrypted_text = caesar_encrypt(&plain_text, key, Language::Ukrainian);
+        assert_eq!(encrypted_text, "ВгҐдЕЄжЗИІЇйКЛМнОПрСТУфХцЧШщЬЮЯАб");
+    }
+
+    #[test]
     fn decrypt_ukr_text() {
         let encrypted_text = "ҐдЕєЖЗиІЇЙКлМНОпРСтУФХцЧшЩЬюЯАБВг";
         let key = 4;
+        let dencrypted_text = caesar_decrypt(&encrypted_text, key, Language::Ukrainian);
+        assert_eq!(dencrypted_text, "АбВгҐДеЄЖЗИіЇЙКлМНоПРСтУфХЦчШЩЬЮя");
+    }
+
+    #[test]
+    fn decrypt_ukr_text_key_overflow() {
+        let encrypted_text = "ВгҐдЕЄжЗИІЇйКЛМнОПрСТУфХцЧШщЬЮЯАб";
+        let key = 35;
         let dencrypted_text = caesar_decrypt(&encrypted_text, key, Language::Ukrainian);
         assert_eq!(dencrypted_text, "АбВгҐДеЄЖЗИіЇЙКлМНоПРСтУфХЦчШЩЬЮя");
     }
